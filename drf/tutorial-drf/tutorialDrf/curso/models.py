1e8b535c6b31220b9b80b65ae5ba7579014ee425
pedro <pkovokar@gmail.com>
2025-03-18 16:47:23 -0300

    avaçiação

--- drf/tutorial-drf/tutorialDrf/curso/models.py
+++ drf/tutorial-drf/tutorialDrf/curso/models.py
@@ -1,4 +1,5 @@
-from django.db import models
+from django.db import models, CheckConstraint, Q
+
 
 # Create your models here.
 class Base(models.Model):
@@ -26,4 +27,14 @@ class Avaliacao(Base):
     curso = models.ForeignKey(Curso, related_name='avaliacoes', on_delete=models.CASCADE)
     nome = models.CharField(max_length=255)
     email = models.EmailField()
-    comentario  = models
+    comentario  = models.TextField(blank=True, default="")
+    avaliacao = models.DecimalField(max_digits=2, decimal_places=1)
+
+    class Meta:
+        verbose_name = "Avaliação"
+        verbose_name_plural = "Avaliações"
+        unique_together = ["email", "curso"]
+        constraints = [CheckConstraint(check=Q(avaliacao__gt=0), name='avaliacao_valida')]
+    
+    def __str__(self):
+        return f'{self.nome} avaliou o curso {self.curso} com nota = {self.avaliacao}'
