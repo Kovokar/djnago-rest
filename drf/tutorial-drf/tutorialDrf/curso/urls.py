bcfac4e1d0fd97c984c28c4eac2629cc39565fdb
pedro <pkovokar@gmail.com>
2025-03-18 19:11:54 -0300

    api funcional porem simples

--- drf/tutorial-drf/tutorialDrf/curso/urls.py
+++ drf/tutorial-drf/tutorialDrf/curso/urls.py
@@ -1,7 +1,18 @@
-from django.urls import path
+from django.urls import path, include
 from .views import CursoAPIView, AvaliacaoAPIView
+from rest_framework.routers import DefaultRouter
+
+# urlpatterns = [
+#     path('cursos/', CursoAPIView.as_view(), name='cursos-list'),  # Endpoint para listagem de cursos
+#     path('cursos/create/', CursoAPIView.as_view(), name='cursos-create'),  # Endpoint para criação de curso
+#     path('avaliacoes/', AvaliacaoAPIView.as_view(), name='avaliacoes-list'),  # Endpoint para listagem de avaliações
+# ]
+
+
+router = DefaultRouter()
+router.register(r'cursos', CursoAPIView)
+router.register(r'avaliacoes', AvaliacaoAPIView)
 
 urlpatterns = [
-    path('cursos/', CursoAPIView.as_view(), name='cursos'),
-    path('avaliacoes/', AvaliacaoAPIView.as_view(), name='avaliacoes'),
+    path('', include(router.urls)),
 ]
--- drf/tutorial-drf/tutorialDrf/curso/views.py
+++ drf/tutorial-drf/tutorialDrf/curso/views.py
@@ -1,19 +1,25 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from .models import Curso, Avaliacao
+from .serializers import CursoSerializer, AvaliacaoSerializer
 from rest_framework.response import Response
-from .serializers import *
-from .models import *
 
-class CursoAPIView(APIView):
-    def get(self, request):
-        cursos = Curso.objects.all()
-        serializer = CursoSerializer(cursos, many=True)
+
+class CursoAPIView(viewsets.ModelViewSet):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
+    @action(detail=False, methods=['get'], url_path='curso-mais-avaliado')
+    def curso_mais_avaliado(self, request):
+        # Exemplo de lógica personalizada para pegar o curso mais avaliado
+        curso = Curso.objects.order_by('-avaliacoes__avaliacao').first()  # Exemplo de lógica
+        print(curso)
+        serializer = self.get_serializer(curso)
+        # serializer = self.get_serializer(curso)
         return Response(serializer.data)
 
-class AvaliacaoAPIView(APIView):
-    def get(self, request):
-        avaliacoes = Avaliacao.objects.all()
-        serializer = AvaliacaoSerializer(avaliacoes, many=True)
-        return Response(serializer.data)
     
-# Create your views here.
+class AvaliacaoAPIView(viewsets.ModelViewSet):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
+
