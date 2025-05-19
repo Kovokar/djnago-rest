9cffbc0ca66a418f6d43060cac73f47eefc1575f
estagiario.devmed <estagiario.dev.medclub@gmail.com>
2025-05-19 11:43:27 -0300

    q10

--- sql/trigger/novasQueotesGpt/q10.sql
+++ sql/trigger/novasQueotesGpt/q10.sql
@@ -1,2 +1,27 @@
--- Q10: Reduzir preço de livros com grande estoque
--- Escreva sua consulta SQL aqui
+--10. Diminua o valor unitário de todos os 
+--livros em 10% se o estoque estiver acima de 100 unidades.
+
+create or replace function diminui_preco_baseado_no_estoque()
+returns trigger as $$
+declare 
+	tot numeric;
+begin
+	select sum(l.qtd_estoque) into tot from livro l;
+	if tot > 100 then
+		raise notice 'aaa';
+		new.valor_livro := new.valor_livro * 0.9;
+	end if;
+	return new;
+end;
+$$ language plpgsql;
+
+
+create trigger diminui_preco_baseado_no_estoque2
+after update on livro
+for each row
+execute function diminui_preco_baseado_no_estoque();
+
+
+update livro l
+set qtd_estoque = 12
+where l.cod_livro = 1
