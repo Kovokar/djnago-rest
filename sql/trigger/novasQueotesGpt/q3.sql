7b5d06db704c18c0e4d02efd610a3303a0681610
kovokar <pkovokar@gmail.com>
2025-05-19 00:45:14 -0300

    q3

--- sql/trigger/novasQueotesGpt/q3.sql
+++ sql/trigger/novasQueotesGpt/q3.sql
@@ -1,2 +1,19 @@
--- Q3: Pedidos por fornecedor
--- Escreva sua consulta SQL aqui
+--3. Liste todos os pedidos feitos por cada fornecedor, 
+--incluindo a data, o número de itens e o valor total.
+
+select * from pedido p 
+inner join fornecedor f 
+on f.cod_fornecedor = p.cod_fornecedor 
+order by p.cod_fornecedor 
+
+SELECT * FROM TITULO order by cod_titulo ;
+
+SELECT * FROM LIVRO order by cod_livro 
+
+SELECT * FROM FORNECEDOR;
+
+SELECT * FROM PEDIDO P order by p.cod_pedido ;
+
+SELECT * FROM ITEM_PEDIDO;
+
+SELECT * FROM CONTROLE_DE_ALTERACAO;
