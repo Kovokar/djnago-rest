20638d37b2720b7f1585f27f46165c9beff24fe1
kovokar <pkovokar@gmail.com>
2025-05-19 01:05:11 -0300

    q5

--- sql/trigger/novasQueotesGpt/q5.sql
+++ sql/trigger/novasQueotesGpt/q5.sql
@@ -1,2 +1,6 @@
--- Q5: Títulos nunca pedidos
--- Escreva sua consulta SQL aqui
+--5. Liste os títulos que nunca foram pedidos.
+
+select * from livro l 
+where cod_livro not in (
+	select cod_livro from pedido p 
+)
