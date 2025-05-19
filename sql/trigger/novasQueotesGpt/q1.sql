71226e570212c6806493ef4e4d566733d813ac5e
kovokar <pkovokar@gmail.com>
2025-05-19 00:04:46 -0300

    q1

--- sql/trigger/novasQueotesGpt/q1.sql
+++ sql/trigger/novasQueotesGpt/q1.sql
@@ -1,2 +1,5 @@
--- Q1: Títulos com menos de 5 unidades em estoque
--- Escreva sua consulta SQL aqui
+--1. Liste todos os títulos de livros que possuem menos de 5 unidades em estoque.
+
+select t.nome_titulo from titulo t 
+where qtd_titulos < 15
+
