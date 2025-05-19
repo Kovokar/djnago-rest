0f88d4c3140105e40baf0d0aa38deed70c4f426a
estagiario.devmed <estagiario.dev.medclub@gmail.com>
2025-05-19 10:38:00 -0300

    q7

--- sql/trigger/novasQueotesGpt/q7.sql
+++ sql/trigger/novasQueotesGpt/q7.sql
@@ -1,2 +1,11 @@
--- Q7: Fornecedores com valor médio por item pedido superior a R$ 100,00
--- Escreva sua consulta SQL aqui
+--7. Liste os fornecedores cujo valor médio por
+--item pedido é superior a R$ 210,00.
+
+
+select f.nome_fornecedor from fornecedor f 
+where f.cod_fornecedor in (
+	select p.cod_fornecedor from pedido p
+	group by p.cod_fornecedor
+	having sum(p.valor_total_pedido)/ COUNT(p.cod_fornecedor) > 210
+	order by p.cod_fornecedor
+)
