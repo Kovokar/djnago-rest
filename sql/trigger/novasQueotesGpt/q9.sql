fcdf1835008e2e65df10b198fb80a5051ceab344
estagiario.devmed <estagiario.dev.medclub@gmail.com>
2025-05-19 11:02:24 -0300

    q9

--- sql/trigger/novasQueotesGpt/q9.sql
+++ sql/trigger/novasQueotesGpt/q9.sql
@@ -1,2 +1,30 @@
--- Q9: Atualizar estoque com base nos pedidos
--- Escreva sua consulta SQL aqui
+--9. Atualize o estoque dos livros de acordo com os pedidos realizados.
+
+
+create or replace function att_estoque()
+returns trigger as $$
+
+begin
+	update LIVRO 
+	set qtd_estoque = qtd_estoque - 1
+	where cod_livro = new.cod_livro;
+	
+	raise notice 'estoque decrementado';
+	return new;
+end;
+$$ language plpgsql;
+
+
+create trigger att_estoque
+after insert on item_pedido
+for each row 
+execute function att_estoque()
+
+
+
+insert into pedido (cod_pedido, cod_fornecedor, hora_pedido, data_pedido, valor_total_pedido)
+values (19, 4, '12:12:00', now(), 180)
+
+
+insert into item_pedido (cod_item_pedido, cod_pedido, cod_livro)
+values (19, 19, 5)
