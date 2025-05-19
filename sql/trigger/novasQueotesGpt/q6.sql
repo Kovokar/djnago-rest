f0e35b44ecedfd033a8023caa6ad3a270a17d858
kovokar <pkovokar@gmail.com>
2025-05-19 01:19:28 -0300

    q6

--- sql/trigger/novasQueotesGpt/q6.sql
+++ sql/trigger/novasQueotesGpt/q6.sql
@@ -1,2 +1,16 @@
--- Q6: Fornecedor que vendeu mais livros em quantidade em fevereiro de 2024
--- Escreva sua consulta SQL aqui
+--6. Mostre o nome do fornecedor que vendeu mais livros em 
+--uantidade (e não em valor) em fevereiro de 2024
+
+
+select * from fornecedor f 
+where f.cod_fornecedor in (
+	select p.cod_fornecedor from pedido p 
+	group by p.cod_fornecedor 
+	having count(p.cod_fornecedor) in (
+		select count(p.cod_fornecedor)  from pedido p 
+		group by p.cod_fornecedor 
+		order by count(p.cod_fornecedor) desc 
+		limit 1
+	)
+	order by count(p.cod_fornecedor) desc 
+)
